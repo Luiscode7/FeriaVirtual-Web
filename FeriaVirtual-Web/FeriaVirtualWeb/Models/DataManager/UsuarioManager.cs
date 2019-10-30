d0c4ec719c6fbebb03bf0a164892801a3fd51f5f
luis venegas <luiseduardo.venegas7@gmail.com>
2019-10-30 15:55:15 -0300

    *se agrega encriptacion de password MD5

--- FeriaVirtual-Web/FeriaVirtualWeb/Models/DataManager/UsuarioManager.cs
+++ FeriaVirtual-Web/FeriaVirtualWeb/Models/DataManager/UsuarioManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using FeriaVirtualWeb.Models.DataContext;
 
@@ -13,14 +15,60 @@ namespace FeriaVirtualWeb.Models.DataManager
             var usuario = new USUARIO();
             using (FeriaVirtualEntities db = new FeriaVirtualEntities())
             {
-                
-                if(!string.IsNullOrEmpty(rut) && !string.IsNullOrEmpty(password))
+
+                if (!string.IsNullOrEmpty(rut) && !string.IsNullOrEmpty(password))
                 {
-                    usuario = db.USUARIO.FirstOrDefault(p => p.RUTUSUARIO == rut && p.CONTRASENA == password);
+                    
+                    using (MD5 md5Hash = MD5.Create())
+                    {
+                        string hash = GetMd5Hash(md5Hash, password);
+
+                        if (VerifyMd5Hash(md5Hash, password, hash))
+                        {
+                            usuario = db.USUARIO.FirstOrDefault(p => p.RUTUSUARIO == rut && p.CONTRASENA == hash);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hashes are not same.");
+                        }
+                      
+                    }
+                    
                 }
-                
+
+                return usuario;
             }
-            return usuario;
+            
+        }
+
+        private string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
